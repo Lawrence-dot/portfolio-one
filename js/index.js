188dc7e02cff3b99a764b93a0cfcdd20503ff19f
Lawrence-dot <damilareojediran3@gmail.com>
2020-10-22 16:56:25 -0700

    Update

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var toggleBtn = document.getElementById("toggle-btn");
+
+
+// toggleBtn.onclick =  function () {
+//     toggleBtn.classList.toggle("cancel");
+//     if (toggleBtn.className == "navbar-toggler") {
+//         console.log("closed");
+//         toggleBtn.className = "navbar-toggler";
+//         toggleBtn.innerHTML = "";
+//     } else {
+//         console.log("opened");
+//         toggleBtn.innerHTML = "&times";
+//         toggleBtn.className = "cancel";
+//     }
+// };
+
